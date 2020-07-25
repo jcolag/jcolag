aee7cc84b4b09c123410fa01ddae79dd9a143e91
John Colagioia <jcolag@colagioia.net>
2020-07-25 06:41:17 -0400

    Update blog posts as of 2020-07-25 morning

--- runupdate.sh
+++ runupdate.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 today=$(date +"%Y-%m-%d")
-yarn install
-git pull
-node update.js
-git add .
-git commit -m "Update blog posts as of ${today} morning"
-git push
+yarn install 2>&1 > results.log
+git pull 2>&1 > results.log
+node update.js 2>&1 > results.log
+git add . 2>&1 > results.log
+git commit -m "Update blog posts as of ${today} morning" 2>&1 > results.log
+git push 2>&1 > results.log
 
